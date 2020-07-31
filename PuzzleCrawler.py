d2bcb14b9ea9a4939b8324688cef325838238133
will <jiayewilliamwang@gmail.com>
2020-07-31 13:05:41 -0600

    Add the colors reference table, and improve the format of column and row colored groups

--- PuzzleCrawler.py
+++ PuzzleCrawler.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 
 class PuzzleCrawler:
     def __init__(self):
-        puzzle_url = "https://www.nonograms.org/nonograms2/i/5660"
+        puzzle_url = "https://www.nonograms.org/nonograms2/i/4374"
         self._driver = webdriver.Chrome()
         self._driver.get(puzzle_url)
 
@@ -11,19 +11,29 @@ class PuzzleCrawler:
         self._col_ele, self._row_ele = self._driver.find_elements_by_xpath(
             '//*[@id="nonogram_table"]/tbody/tr')
 
-        self._puzzle_grid = self.get_puzzle_grid()
-        self._color_table = self.get_color_table()
+        self._puzzle_grid = self._get_puzzle_grid()
+        self._color_table = self._get_color_table()
 
-        self._col_group = self.get_col_groups()
-        self._row_group = self.get_row_groups()
+        self._col_group = self._get_col_groups()
+        self._row_group = self._get_row_groups()
 
-    def get_color_table(self):
+    def _get_color_table(self) -> dict:
+        """
+        Get the color table of the puzzle.
+        Add color white and value 0 as default.
+        :return: {bg_color: i} where bg_color is in "rbga(r, g, b, a)" format.
+        i is in range 0 to n, where n is number of colors, exclusive white.
+        """
         color_table_ele = self._driver.find_elements_by_xpath(
             "//table[@class='nonogram_color_table']/tbody/tr/td")
-        return [color.value_of_css_property("background-color")
-                for color in color_table_ele]
 
-    def get_puzzle_grid(self):
+        color_table = {"rgba(255, 255, 255, 1)": 0}
+        for color in color_table_ele:
+            bg_color = color.value_of_css_property("background-color")
+            color_table[bg_color] = int(color.text)
+        return color_table
+
+    def _get_puzzle_grid(self) -> list:
         """
         Get the grid of the puzzle. It is going to be modified and
         synchronized when solving the puzzle. Moreover, visualize the solving
@@ -36,17 +46,17 @@ class PuzzleCrawler:
         grid = []
         for row in puzzle_grid:
             cur_row = []
-            for cell in row.find_elements_by_xpath(".//td"):
+            for cell in row.find_elements_by_xpath("./td"):
                 cur_row.append(cell)
             grid.append(cur_row)
         return grid
 
-    def get_row_groups(self):
+    def _get_row_groups(self) -> list:
         """
-        Get the vertical block groups length.
-        :return: list of lists of integer that has length _row. Each sublist
-            may not have the same size, but none of them will empty. The
-            summation of each sublist will not be greater than _row.
+        Get the horizontal colored groups.
+        :return: list of lists of pair of integers. Each sublist's length will
+            be at least 1, represents the colored groups of each row. For each
+            color group is in [length: int, color-reference: int] format.
         """
         row_elements = self._row_ele.find_elements_by_xpath(
             "./td[@class='nmtl']/table/tbody/tr")
@@ -56,16 +66,16 @@ class PuzzleCrawler:
             cur_row = []
             for cell in row.find_elements_by_xpath("./td[@class='num']"):
                 bg_color = cell.value_of_css_property("background-color")
-                cur_row.append([bg_color, int(cell.text)])
+                cur_row.append([int(cell.text), self._color_table[bg_color]])
             ans.append(cur_row)
         return ans
 
-    def get_col_groups(self):
+    def _get_col_groups(self) -> list:
         """
-        Get the horizontal block groups length.
-        :return: list of lists of integers that has length _col. Each sublist
-            may not have the same size, but none of them will be empty. The
-            summation of each sublist will not be greater than _col.
+        Get the vertical colored groups.
+        :return: list of lists of pair of integers. Each sublist's length will
+            be at least 1, represents the colored groups of each col. For each
+            color group is in [length: int, color-reference: int] format.
         """
         col_elements = self._col_ele.find_elements_by_xpath(
             "./td[@class='nmtt']/table/tbody/tr")
@@ -76,27 +86,22 @@ class PuzzleCrawler:
             for i in range(len(temp)):
                 if temp[i].get_attribute("class") == "num":
                     bg_color = temp[i].value_of_css_property("background-color")
-                    ans[i].append([bg_color, int(temp[i].text)])
+                    ans[i].append([int(temp[i].text), self._color_table[bg_color]])
         return ans
 
+    # ----- Getters -----
     @property
-    def puzzle_grid(self):
+    def puzzle_grid(self) -> list:
         return self._puzzle_grid
 
     @property
-    def color_table(self):
+    def color_table(self) -> dict:
         return self._color_table
 
     @property
-    def col_group(self):
+    def col_group(self) -> list:
         return self._col_group
 
     @property
-    def row_group(self):
+    def row_group(self) -> list:
         return self._row_group
-
-
-if __name__ == '__main__':
-    obj = PuzzleCrawler()
-    print(obj.col_group)
-
